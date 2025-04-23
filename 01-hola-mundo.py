bd73c16038e3802c75a969fb6d6e4bcd2f701d80
Esteban <esteban2029@gmail.com>
2025-04-22 23:39:07 -0500

    cambio

---
+++ 01-hola-mundo.py
@@ -0,0 +1,12 @@
+nombre = "Esteban"
+print(type(nombre))
+nombre = 10.5
+print(type(nombre))
+
+print("Hola Mundo")
+print(nombre)
+#Pra saber el tipo de variable
+print(type(nombre))
+
+texto = "Buenas noches con todos Brian OConner"
+texto = 
