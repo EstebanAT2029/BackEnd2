17e8ae650ad3f22b63feb5878882c26ad92291c0
Esteban <esteban2029@gmail.com>
2025-04-27 22:48:10 -0500

    dia 01

---
+++ semana01/02-colecciones-de-datos.py
@@ -0,0 +1,14 @@
+#Lista (Arreglo)
+# Modificable y ordenada
+numeros_de_emergencia = ["104", "980874153","979503242", "051352200"]
+
+print(numeros_de_emergencia)
+print(numeros_de_emergencia[0])
+
+longitud_numeros = len(numeros_de_emergencia)
+print(numeros_de_emergencia[longitud_numeros-1])
+#si queremos recorrer la lista de derecha a izquierda podemos usar valores negativos
+print(numeros_de_emergencia[-1])
+#si yo quiero hacer una sub lista de la lista
+#pos_inicio: > desde la pos inicial hasta el final
+print(numeros_de_emergencia[0:])
