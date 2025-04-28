8f59187d12ad9e0b9e28b2461a4ae6b357c9ae0f
Esteban <esteban2029@gmail.com>
2025-04-27 23:16:45 -0500

    antes de ejercicio

--- semana01/02-colecciones-de-datos.py
+++ semana01/02-colecciones-de-datos.py
@@ -10,5 +10,22 @@ print(numeros_de_emergencia[longitud_numeros-1])
 #si queremos recorrer la lista de derecha a izquierda podemos usar valores negativos
 print(numeros_de_emergencia[-1])
 #si yo quiero hacer una sub lista de la lista
-#pos_inicio: > desde la pos inicial hasta el final
-print(numeros_de_emergencia[0:])
+#pos_inicio:            > desde la pos inicial hasta el final
+#pos_inicio: pos_final  > desde la pos inicial hasta la menor que la pos final
+# :pos_final            > desde el inicio hasta la pos final
+print(numeros_de_emergencia[2:])
+print(numeros_de_emergencia[1:3])
+print(numeros_de_emergencia[:2])
+
+##Agregar o Quitar listas
+numeros_de_emergencia.append("9808741522")
+numeros_de_emergencia.append("981612634")
+print(numeros_de_emergencia)
+
+#Remueve el elemento de la vista y los siguientes elementos ocupan su lugar
+numero_eliminado = numeros_de_emergencia.pop(3)
+print(numero_eliminado)
+print(numeros_de_emergencia) 
+# del > delete
+del numeros_de_emergencia[3]
+print(numeros_de_emergencia)
