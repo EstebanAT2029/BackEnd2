5fc318d2c98025d2d232db06dce288fd543ee2cb
Esteban <esteban2029@gmail.com>
2025-04-27 23:41:18 -0500

    ejerciocios2

--- semana01/02-colecciones-de-datos.py
+++ semana01/02-colecciones-de-datos.py
@@ -1,4 +1,4 @@
-#Lista (Arreglo)
+#Lista (Arreglo) se representa por []
 # Modificable y ordenada
 numeros_de_emergencia = ["104", "980874153","979503242", "051352200"]
 
@@ -29,3 +29,110 @@ print(numeros_de_emergencia)
 # del > delete
 del numeros_de_emergencia[3]
 print(numeros_de_emergencia)
+
+#empezar de 0 o resetear 
+numeros_de_emergencia.clear()
+
+# Las listas pueden tener diferentes tipos de datos dentro de ella
+miscelaneo = ['Roberto', '2015-02-05', False, 10, 14.5, -10]
+
+ejercicio_1 = [1, 'Luis', 'Marcona', False, 80, 20.5, [4, 5, 6]]
+# Como hago para obtener a 'Marcona'
+resultado_1 = ejercicio_1[2]
+
+# Como hago para obtener 'Luis' hasta 80
+resultado_2 = ejercicio_1[1:5]
+
+# Como hago para obtener la penultima posicion
+resultado_3 = ejercicio_1[-2]
+
+# Como hago para obtener el numero 5
+resultado_4 = ejercicio_1[6][1]
+
+extraer = ejercicio_1[6]
+print(extraer)
+print(extraer[1])
+
+print(resultado_1)
+print(resultado_2)
+print(resultado_3)
+print(resultado_4)
+
+miscelaneo[0] = '99999999'
+
+#### Tuplas, se representa mediante parentesis
+# Son ordenadas PERO no son editables
+meses = ('Enero', 'Febrero', 'Marzo')
+
+# Definir informacion quen o va a cambiar en todo el tiempo de la aplicacion
+
+print(meses[2])
+print(meses[-1])
+
+# Si en la tupla tenemos una lista, esa lista si se puede modificar
+# meses[0] = 'Diciembre'
+
+data = ('Juan', 'Roberto', [1, 2, 3, ['Eduardo', 'Arnold']])
+
+# Como hago para obtener Eduardo
+resultado = data[2][3][0]
+print(resultado)
+
+# Diccionarios
+# Semi ordenado porque su orden depende las llaves (keys) y a su vez es editable
+persona = {
+    'nombre': 'Eduardo',
+    'apellido': 'Bareto',
+    'edad': 31,
+    'hobbies': ['Natacion', 'Trekking', 'Programacion'],
+    'idiomas': ('Español', 'Ingles'),
+    'habilidades': [
+        {
+            'nombre': 'Honestidad',
+            'importancia': 'Alta'
+        },
+        {
+            'nombre': 'Generocidad',
+            'importancia': 'Media'
+        },
+        {
+            'nombre': 'Empatia',
+            'importancia': 'Media Alta'
+        }
+    ]
+}
+
+print(persona)
+
+# Editar llaves existentes
+persona['nombre'] = 'Shrek'
+print(persona)
+# La llave que quiero editar no existe, se creara
+persona['nacionalidad'] = 'Peruano'
+print(persona)
+
+del persona['apellido']
+elemento_eliminado = persona.pop('edad')
+print(persona)
+print(elemento_eliminado)
+
+# Para limpiar todo el diccionario
+# persona.clear()
+print(persona)
+
+# Si una llave contiene una tupla que no es editable no se puede ni agregar ni eliminar elementos de
+# persona['idiomas'].append('Frances')
+
+persona['hobbies'].append('Trabajar')
+
+# Conjunto (Set)
+# Desordenada y es editable
+planetas = {'Tierra', 'Marte', 'Jupiter', 'Saturno'}
+
+planetas.add('Urano')
+
+print(planetas)
+
+# in > sirve para poder ubicar un elemento dentro de una coleccion de dato, una lista, tupla o conjunto
+print('Tierra' in planetas)
+print('Pluton' in planetas)
